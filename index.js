cf8055f58a44a66aa0e4b407a908668bc4c04873
ajay9803 <ajayprajapati9803@gmail.com>
2024-06-04 03:42:09 +0545

    Update sass codes / refactor

--- index.js
+++ index.js
@@ -1,178 +1,78 @@
-console.log("hello world");
+// dark mode actions
 
 let darkMode = true;
 
 const toggleTheme = () => {
   darkMode = !darkMode;
-  console.log("Dark mode is: ", darkMode);
   updateTheme();
 };
 
 const updateTheme = () => {
+  const body = document.getElementById("body");
   const topSection = document.getElementById("top-section");
   const bottomSection = document.getElementById("bottom-section");
-  const toggleButton = document.getElementById("toggle-button-1");
-  const input = document.getElementById("search-input");
   if (darkMode) {
-    topSection.classList.add("top-section--dark");
-    topSection.classList.remove("top-section--light");
-    bottomSection.classList.add("bottom-section--dark");
-    bottomSection.classList.remove("bottom-section--light");
-    toggleButton.classList.add("background--dark");
-    toggleButton.classList.remove("background--light");
-    input.classList.remove("input--shadow");
-    input.classList.add("input--noshadow");
+    body.classList.add("body__dark");
+    body.classList.remove("body__light");
+
+    topSection.classList.add("body__top-section--dark");
+    topSection.classList.remove("body__top-section--light");
+    bottomSection.classList.add("body__bottom-section--dark");
+    bottomSection.classList.remove("body__bottom-section--light");
   } else {
-    topSection.classList.add("top-section--light");
-    topSection.classList.remove("top-section--dark");
-    bottomSection.classList.add("bottom-section--light");
-    bottomSection.classList.remove("bottom-section--dark");
-    toggleButton.classList.add("background--light");
-    toggleButton.classList.remove("background--dark");
-    input.classList.add("input--shadow");
-    input.classList.remove("input--noshadow");
+    body.classList.add("body__light");
+    body.classList.remove("body__dark");
+
+    topSection.classList.add("body__top-section--light");
+    topSection.classList.remove("body__top-section--dark");
+    bottomSection.classList.add("body__bottom-section--light");
+    bottomSection.classList.remove("body__bottom-section--dark");
   }
-
-  switchIcons();
-  switchToggleImage();
-  switchMovedownButton();
-  toggleMenuIcons();
-  toggleProductBackground();
-  // handleResize();
-};
-
-const switchToggleImage = () => {
-  let moonImage = document.getElementById("moon-image");
-  let sunImage = document.getElementById("sun-image");
-  if (darkMode) {
-    moonImage.classList.add("item-display");
-    moonImage.classList.remove("item-display--none");
-    sunImage.classList.add("item-display--none");
-    sunImage.classList.remove("item-display");
-  } else {
-    moonImage.classList.add("item-display--none");
-    moonImage.classList.remove("item-display");
-    sunImage.classList.add("item-display");
-    sunImage.classList.remove("item-display--none");
-  }
-};
-
-const switchIcons = () => {
-  let searchLight = document.getElementById("search-light");
-  let searchDark = document.getElementById("search-dark");
-  let cartLight = document.getElementById("cart-light");
-  let cartDark = document.getElementById("cart-dark");
-
-  if (darkMode) {
-    if (innerWidth > 1024) {
-      searchLight.classList.add("item-display");
-      searchLight.classList.remove("item-display--none");
-      searchDark.classList.add("item-display--none");
-      searchDark.classList.remove("item-display");
-    }
-
-    cartLight.classList.add("item-display");
-    cartLight.classList.remove("item-display--none");
-    cartDark.classList.add("item-display--none");
-    cartDark.classList.remove("item-display");
-  } else {
-    if (innerWidth > 1024) {
-      searchLight.classList.add("item-display--none");
-      searchLight.classList.remove("item-display");
-      searchDark.classList.add("item-display");
-      searchDark.classList.remove("item-display--none");
-    }
-
-    cartLight.classList.add("item-display--none");
-    cartLight.classList.remove("item-display");
-    cartDark.classList.add("item-display");
-    cartDark.classList.remove("item-display--none");
-  }
-};
-
-const switchMovedownButton = () => {
-  let lightArrow = document.getElementById("light-arrow");
-  let darkArrow = document.getElementById("dark-arrow");
-  let movedownbutton = document.getElementById("move-down-button");
-  if (darkMode) {
-    movedownbutton.classList.add("border--light");
-    movedownbutton.classList.remove("border--dark");
-    lightArrow.classList.add("item-display");
-    lightArrow.classList.remove("item-display--none");
-    darkArrow.classList.add("item-display--none");
-    darkArrow.classList.remove("item-display");
-  } else {
-    movedownbutton.classList.remove("border--light");
-    movedownbutton.classList.add("border--dark");
-    lightArrow.classList.add("item-display--none");
-    lightArrow.classList.remove("item-display");
-    darkArrow.classList.add("item-display");
-    darkArrow.classList.remove("item-display--none");
-  }
-};
-
-const toggleMenuIcons = () => {
-  let darkMenu = document.getElementById("dark-menu");
-  let lightMenu = document.getElementById("light-menu");
-  if (darkMode) {
-    lightMenu.classList.add("item-display");
-    lightMenu.classList.remove("item-display--none");
-    darkMenu.classList.remove("item-display");
-    darkMenu.classList.add("item-display--none");
-  } else {
-    lightMenu.classList.remove("item-display");
-    lightMenu.classList.add("item-display--none");
-    darkMenu.classList.add("item-display");
-    darkMenu.classList.remove("item-display--none");
-  }
-};
-
-const toggleProductBackground = () => {
-  let products = document.querySelectorAll(".product");
-
-  products.forEach((product) => {
-    if (darkMode) {
-      product.classList.remove("product--dark-back");
-      product.classList.add("product--light-back");
-    } else {
-      product.classList.add("product--dark-back");
-      product.classList.remove("product--light-back");
-    }
-  });
 };
 
 document.getElementById("toggle-button").addEventListener("click", toggleTheme);
 updateTheme();
-switchIcons();
-switchToggleImage();
-switchMovedownButton();
-toggleMenuIcons();
-toggleProductBackground();
 
-// resize window event listener
+// menu bar actions
 
-const handleResize = () => {
-  console.log("handle resize");
-  let searchLight = document.getElementById("search-light");
-  let searchDark = document.getElementById("search-dark");
-  if (innerWidth > 1024) {
-    if (darkMode) {
-      searchDark.classList.remove("item-display");
-      searchDark.classList.add("item-display--none");
-      searchLight.classList.remove("item-display--none");
-      searchLight.classList.add("item-display");
-    } else {
-      searchDark.classList.remove("item-display--none");
-      searchDark.classList.add("item-display");
-      searchLight.classList.remove("item-display");
-      searchLight.classList.add("item-display--none");
-    }
+let showMenuBar = false;
+
+console.log('show menu bar is', showMenuBar);
+
+const toggleMenuBar = () => {
+  showMenuBar = !showMenuBar;
+  console.log("menu button cliked");
+  let popUpMenu = document.getElementById("menu-pop-up");
+  let darkWrapper = document.getElementById("dark-wrapper");
+  if (showMenuBar) {
+    popUpMenu.classList.add("item-display");
+    popUpMenu.classList.remove("item-display--none");
+    darkWrapper.classList.add("item-display");
+    darkWrapper.classList.remove("item-display--none");
   } else {
-    searchDark.classList.add("item-display--none");
-    searchDark.classList.remove("item-display");
-    searchLight.classList.remove("item-display");
-    searchLight.classList.add("item-display--none");
+    popUpMenu.classList.remove("item-display");
+    popUpMenu.classList.add("item-display--none");
+    darkWrapper.classList.remove("item-display");
+    darkWrapper.classList.add("item-display--none");
   }
 };
 
-window.addEventListener("resize", handleResize);
+document.getElementById("menu-button").addEventListener("click", toggleMenuBar);
+
+addEventListener("resize", () => {
+  console.log("here");
+  let popUpMenu = document.getElementById("menu-pop-up");
+  let darkWrapper = document.getElementById("dark-wrapper");
+  if (innerWidth > 1024) {
+    showMenuBar = false;
+    console.log(showMenuBar, "is false");
+    popUpMenu.classList.remove("item-display");
+    popUpMenu.classList.add("item-display--none");
+    darkWrapper.classList.remove("item-display");
+    darkWrapper.classList.add("item-display--none");
+  }
+});
+
+// document
+//   .getElementById("dark-wrapper")
+//   .addEventListener("click", toggleMenuBar);
